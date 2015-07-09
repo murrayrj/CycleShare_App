60635736a89770400c05a89fdfa5b56e0eb25da9
Richard Murray-Clark <richardmurray-clark@hotmail.com>
2015-07-09 13:11:00 +0100

    Added changes to User and created Identties

--- bike-sharing-server/db/migrate/20150709120424_add_changes_to_user.rb
+++ bike-sharing-server/db/migrate/20150709120424_add_changes_to_user.rb
@@ -1,7 +1,6 @@
 class AddChangesToUser < ActiveRecord::Migration
   def change
     add_column :users, :name, :string
-    add_column :users, :email, :string
     add_column :users, :phone, :string
   end
 end
--- bike-sharing-server/db/schema.rb
+++ bike-sharing-server/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150708144012) do
+ActiveRecord::Schema.define(version: 20150709120625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,12 +26,35 @@ ActiveRecord::Schema.define(version: 20150708144012) do
     t.integer  "renter_id"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "identities", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "provider"
+    t.string   "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  add_index "identities", ["user_id"], name: "index_identities_on_user_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
     t.string   "name"
-    t.string   "email"
     t.string   "phone"
   end
 
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  add_foreign_key "identities", "users"
 end
