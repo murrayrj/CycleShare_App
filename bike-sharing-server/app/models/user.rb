b8bfdaa5fe21ca3e8dc7fe5414a6958935e057e8
Richard Murray-Clark <richardmurray-clark@hotmail.com>
2015-07-09 14:47:11 +0100

    Added from_omniauth method to user.rb

--- bike-sharing-server/app/models/user.rb
+++ bike-sharing-server/app/models/user.rb
@@ -6,4 +6,13 @@ class User < ActiveRecord::Base
          :omniauthable, :omniauth_providers => [:facebook]
   has_many :bikes_as_owner, class_name: "Bike", foreign_key: "owner_id"
   has_many :bikes_as_renter, class_name: "Bike", foreign_key: "renter_id"
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name   # assuming the user model has a name
+      user.image = auth.info.image # assuming the user model has an image
+    end
+  end
 end
