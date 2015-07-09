ac5574dc5b5af6333c42c06cc25a009b0a944d19
Richard Murray-Clark <richardmurray-clark@hotmail.com>
2015-07-09 13:01:39 +0100

    Partway through devise setup for rails

--- bike-sharing-server/app/models/user.rb
+++ bike-sharing-server/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :bikes_as_owner, class_name: "Bike", foreign_key: "owner_id"
   has_many :bikes_as_renter, class_name: "Bike", foreign_key: "renter_id"
 end
