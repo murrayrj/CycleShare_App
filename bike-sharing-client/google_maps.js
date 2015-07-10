96753c6a0acf70e1decd471289caf28e01e7cbf0
Richard Murray-Clark <richardmurray-clark@hotmail.com>
2015-07-10 15:11:33 +0100

    Separated javascript and html maps files

---
+++ bike-sharing-client/google_maps.js
@@ -0,0 +1,24 @@
+var geocoder;
+var map;
+function initialize() {
+  geocoder = new google.maps.Geocoder();
+  var latlng = new google.maps.LatLng(51.50722, -0.12750);
+  var mapOptions = {
+    zoom: 11,
+    center: latlng
+  }
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+}
+
+function searchLocation() {
+  var locationQuery = document.getElementById('search-box').value;
+  geocoder.geocode( { 'address': locationQuery}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
