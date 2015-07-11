6a4db8c96cff4290ca28767fa6426221b120c214
Richard Murray-Clark <richardmurray-clark@hotmail.com>
2015-07-11 17:04:20 +0100

    Added CORS

--- bike-sharing-server/app/controllers/bikes_controller.rb
+++ bike-sharing-server/app/controllers/bikes_controller.rb
@@ -1,2 +1,6 @@
 class BikesController < ApplicationController
+  def index
+    @bikes = Bike.all
+    render json: @bikes
+  end
 end
--- bike-sharing-server/config/routes.rb
+++ bike-sharing-server/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   resources :bikes
   resources :users
-  root 'users#index'
+  root 'bikes#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
   # Example of named route that can be invoked with purchase_url(id: product.id)
