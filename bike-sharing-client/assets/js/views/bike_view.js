a458679e30959a893862e6a850ce9b3689c03303
murrayrj <richardmurray-clark@hotmail.com>
2015-07-11 19:41:42 +0100

    Updated bike_view.js

--- bike-sharing-client/assets/js/views/bike_view.js
+++ bike-sharing-client/assets/js/views/bike_view.js
@@ -1,13 +1,20 @@
+var i;
 var postcodes = [];
 cycleshareApp.BikeView = Backbone.View.extend({
   el: '#bike',
   render: function () {
-    console.log('render called');
     var postcodeList = $('#postcodeList');
     postcodeList.empty();
     this.collection.each(function (bike) {
       postcodes.push(bike.attributes.postcode);
-      postcodeList.append("<p>" + bike.attributes.postcode+ "</p>");
+      postcodeList.append("<p>" + bike.attributes.postcode + "</p>");
     });
+    for (i = 0; i < postcodes.length; i++) {
+      $.ajax({
+        url: 'https://maps.googleapis.com/maps/api/geocode/json?components=postal_code%3A' + postcodes[i] + '&key=' + Keys.google_maps
+      }).done(function (response) {
+        console.log(response.results[0].geometry.location);
+      });
+    }
   },
 });
