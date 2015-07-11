bc534fd442ca68ed5140931cb5e11bb6ebe6741d
Richard Murray-Clark <richardmurray-clark@hotmail.com>
2015-07-11 23:48:02 +0100

    Postcode list no longer on page

--- bike-sharing-client/assets/js/views/bike_view.js
+++ bike-sharing-client/assets/js/views/bike_view.js
@@ -7,11 +7,8 @@ var map;
 cycleshareApp.BikeView = Backbone.View.extend({
   el: '#bike',
   render: function () {
-    var postcodeList = $('#postcodeList');
-    postcodeList.empty();
     this.collection.each(function (bike) {
       postcodes.push(bike.attributes.postcode);
-      postcodeList.append("<p>" + bike.attributes.postcode + "</p>");
     });
     for (i = 0; i < postcodes.length; i++) {
       $.ajax({
