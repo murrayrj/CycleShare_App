66e5de2424564a5c24b3ed0318434b8b60d88d0a
Richard Murray-Clark <richardmurray-clark@hotmail.com>
2015-07-13 10:58:17 +0100

    Added enum to status attribute of bike

--- bike-sharing-server/app/models/bike.rb
+++ bike-sharing-server/app/models/bike.rb
@@ -1,4 +1,5 @@
 class Bike < ActiveRecord::Base
+  enum status: [ :available, :rented, :unavailable ]
   belongs_to :owner, class_name: "User", foreign_key: "owner_id"
   belongs_to :renter, class_name: "User", foreign_key: "renter_id"
 end
