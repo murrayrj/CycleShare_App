1fa15bf7b3b8cfe3580a7494c6bc5feaa51dfdca
Richard Murray-Clark <richardmurray-clark@hotmail.com>
2015-07-16 10:27:32 +0100

    Added gray icon for unavailable

--- bike-sharing-client/assets/js/map/set_icon_color.js
+++ bike-sharing-client/assets/js/map/set_icon_color.js
@@ -1,6 +1,9 @@
 function setIconColor(marker) {
   if (marker.status === "available") {
     marker.color = "blue";
+  }
+  if (marker.status === "unavailable") {
+    marker.color = "gray";
   } else {
     marker.color = "red";
   }
